3cea82750927b58df5a8d3835778d7b59c2e9ce2
Bingxin Lu <soulsophia@gmail.com>
2018-01-30 16:39:24 +0800

    Add more WES simulators
    Incorporate wessim and gemsim into module fa2wes.

--- csite/fa2wes.py
+++ csite/fa2wes.py
@@ -33,8 +33,8 @@ def check_input(args):
             'Can not find normal.parental_{}.fa under the normal directory: {}'.format(
                 parental, args.normal)
 
-# tumor directory and chain directory must exist.
-# also file chain_dir/tip_node_sample.count.
+    # tumor directory and chain directory must exist.
+    # also file chain_dir/tip_node_sample.count.
     assert os.path.isdir(
         args.chain), '{} is not exist or not a folder.'.format(args.chain)
     assert os.path.isfile(args.chain + '/tip_node_sample.count'),\
@@ -42,6 +42,8 @@ def check_input(args):
             args.chain)
     assert os.path.isdir(
         args.tumor), '{} is not exist or not a folder.'.format(args.chain)
+
+    assert args.wes in ["capsim","wessim","capgem"], "The specified simulator {} is not supported".format(args.wes)
 
 
 def tip_node_leaves_counting(f=None):
@@ -72,32 +74,56 @@ def genomesize(fasta=None):
 
 def compute_normal_gsize(normal_dir):
     normal_gsize = 0
-    for parental in 0, 1:
-        normal_gsize += genomesize(
-            fasta='{}/normal.parental_{}.fa'.format(normal_dir, parental))
+    gsize_file = os.path.join(normal_dir, 'normal_gsize.txt')
+    if os.path.exists(gsize_file):
+        with open(gsize_file,'r') as fin:
+            normal_gsize = int(fin.read().strip())
+    else:
+        for parental in 0, 1:
+            normal_gsize += genomesize(
+                fasta='{}/normal.parental_{}.fa'.format(normal_dir, parental))
+        with open(gsize_file,'w') as fout:
+            line = '{}\n'.format(normal_gsize)
+            fout.write(line)
+
     return normal_gsize
 
 
 def compute_tumor_dna(tumor_dir, tip_node_leaves):
     tumor_dna = 0
     tip_node_gsize = {}
-    for tip_node, leaves in tip_node_leaves.items():
-        # The value of tip_node_gsize[tip_node] is a list of three elements:
-        # 0)genomesize of parental 0
-        # 1)genomesize of parental 1
-        # 2)the sum of parental 0 and 1
-        tip_node_gsize[tip_node] = []
+    gsize_file = os.path.join(tumor_dir, 'tumor_gsize.txt')
+    if os.path.exists(gsize_file):
+        with open(gsize_file,'r') as fin:
+            tumor_dna = int(fin.readline().strip())
+            for line in fin:
+                fields = line.strip().split('\t')
+                tip_node_gsize[fields[0]] = [int(fields[1]), int(fields[2]), int(fields[3])]
+    else:
+        for tip_node, leaves in tip_node_leaves.items():
+            # The value of tip_node_gsize[tip_node] is a list of three elements:
+            # 0)genomesize of parental 0
+            # 1)genomesize of parental 1
+            # 2)the sum of parental 0 and 1
+            tip_node_gsize[tip_node] = []
 
-        for parental in 0, 1:
-            assert os.path.isfile('{}/{}.parental_{}.fa'.format(tumor_dir, tip_node, parental)),\
-                'Can not find {}.parental_{}.fa under the tumor directory: {}'.format(
-                    tip_node, parental, tumor_dir)
-            tip_node_gsize[tip_node].append(genomesize(
-                fasta='{}/{}.parental_{}.fa'.format(tumor_dir, tip_node, parental)))
+            for parental in 0, 1:
+                assert os.path.isfile('{}/{}.parental_{}.fa'.format(tumor_dir, tip_node, parental)),\
+                    'Can not find {}.parental_{}.fa under the tumor directory: {}'.format(
+                        tip_node, parental, tumor_dir)
+                tip_node_gsize[tip_node].append(genomesize(
+                    fasta='{}/{}.parental_{}.fa'.format(tumor_dir, tip_node, parental)))
 
-        tip_node_gsize[tip_node].append(
-            tip_node_gsize[tip_node][0] + tip_node_gsize[tip_node][1])
-        tumor_dna += tip_node_gsize[tip_node][2] * tip_node_leaves[tip_node]
+            tip_node_gsize[tip_node].append(
+                tip_node_gsize[tip_node][0] + tip_node_gsize[tip_node][1])
+            tumor_dna += tip_node_gsize[tip_node][2] * tip_node_leaves[tip_node]
+
+        with open(gsize_file,'w') as fout:
+            line = '{}\n'.format(tumor_dna)
+            fout.write(line)
+            for key, val in tip_node_gsize.items():
+                line = '{}\t{}\t{}\t{}\n'.format(key, val[0], val[1], val[2])
+                fout.write(line)
 
     return tip_node_gsize, tumor_dna
 
@@ -233,28 +259,38 @@ def prepare_sample_tumor(sample_file, args, total_cells, normal_cells, normal_gs
 
 
 
-def run_snakemake(outdir, args, jobs, sample_file, snake_file, cluster_file):
+def run_snakemake(outdir, args, jobs, sample_file, snake_file):
     stddir = os.path.join(outdir, 'stdout')
     if not os.path.exists(stddir):
         os.makedirs(stddir)
 
-    snake_file_copy = os.path.join(outdir, 'config/Snakefile')
-    cluster_file_copy = os.path.join(outdir, 'config/cluster.yaml')
-    # Copy Snakefile and cluster.yaml to the output folder
+    if args.wes == "capsim":
+        snake_file_copy = os.path.join(outdir, 'config/Snakefile_capsim')
+    elif args.wes == "wessim":
+        snake_file_copy = os.path.join(outdir, 'config/Snakefile_wessim')
+    else:
+        snake_file_copy = os.path.join(outdir, 'config/Snakefile')
+    # Copy Snakefile  to the output folder
     shutil.copyfile(snake_file, snake_file_copy)
-    shutil.copyfile(cluster_file, cluster_file_copy)
 
-    orig_params = args.capsim.split()
+    orig_params = args.snakemake.split()
     config = ''
     if '--config' in orig_params:
         cfg_index = orig_params.index('--config')
         config += orig_params[cfg_index+1]
         del orig_params[cfg_index]
         del orig_params[cfg_index]
-    config += ' rlen=' + str(args.read_length) + ' seed=' + str(numpy.random.randint(MAX_INT))
-    cluster = '\"qsub -V -l mem_free={cluster.mem},h_rt={cluster.time} -pe OpenMP {cluster.n} -o ' + os.path.abspath(stddir) + ' -e '  + os.path.abspath(stddir) +'\"'
-
-    final_cmd_params =  orig_params + ['-s', os.path.abspath(snake_file_copy), '-d', os.path.abspath(outdir), '--cluster-config', cluster_file_copy,  '--cluster', cluster, '--configfile', os.path.abspath(sample_file), '--jobs', str(jobs),  '--config', config]
+    config += ' rlen=' + str(args.read_length)
+     # + ' seed=' + str(numpy.random.randint(MAX_INT))
+    if args.use_cluster:
+        cluster_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/cluster.yaml')
+        assert os.path.isfile(cluster_file), 'Cannot find cluster.yaml under the program directory'
+        cluster_file_copy = os.path.join(outdir, 'config/cluster.yaml')
+        shutil.copyfile(cluster_file, cluster_file_copy)
+        cluster = '\"qsub -V -l mem_free={cluster.mem},h_rt={cluster.time} -pe OpenMP {cluster.n} -o ' + os.path.abspath(stddir) + ' -e '  + os.path.abspath(stddir) +'\"'
+        final_cmd_params =  orig_params + ['-s', os.path.abspath(snake_file_copy), '-d', os.path.abspath(outdir), '--cluster-config', cluster_file_copy,  '--cluster', cluster, '--configfile', os.path.abspath(sample_file), '--jobs', str(jobs),  '--config', config]
+    else:
+        final_cmd_params =  orig_params + ['-s', os.path.abspath(snake_file_copy), '-d', os.path.abspath(outdir), '--configfile', os.path.abspath(sample_file), '--jobs', str(jobs),  '--config', config]
     logging.info(' Command: %s', ' '.join(final_cmd_params))
 
     os.system(' '.join(final_cmd_params))
@@ -262,21 +298,22 @@ def run_snakemake(outdir, args, jobs, sample_file, snake_file, cluster_file):
 
 
 def main(progname=None):
-    parse = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description='a wrapper of simulating targeted capture sequencing from reference genome files',
         prog=progname if progname else sys.argv[0])
-    parse.add_argument('-n', '--normal', required=True,
-                       help='the directory of the normal fasta')
-    parse.add_argument('-t', '--tumor', required=True,
-                       help='the directory of the tumor fasta')
-    parse.add_argument('-c', '--chain', required=True,
-                       help='the directory of the tumor chain')
-    parse.add_argument( '--probe', required=True,
+
+    group1 = parser.add_argument_group('Input options')
+    group1.add_argument('-n', '--normal', required=True,
+                       help='the directory of the fasta files of normal genomes')
+    group1.add_argument('-t', '--tumor', required=True,
+                       help='the directory of the fasta files of tumor genomes')
+    group1.add_argument('-c', '--chain', required=True,
+                       help='the directory of the tumor chain files')
+    group1.add_argument( '--probe', required=True,
                        help='the file containing the sequences of target region')
-    default = 'wes_reads'
-    parse.add_argument('-o', '--output', type=str, default=default,
-                       help='output directory [{}]'.format(default))
-    group = parse.add_mutually_exclusive_group()
+
+    group2 = parser.add_argument_group('Parameters for sequencing')
+    group = group2.add_mutually_exclusive_group()
     default = 0
     group.add_argument('-d', '--depth', type=float, default=default,
                        help='the mean depth of tumor for simulating short reads [{}]'.format(default))
@@ -284,36 +321,50 @@ def main(progname=None):
     group.add_argument('-D', '--normal_depth', type=float, default=default,
                        help='the mean depth of normal for simulating short reads [{}]'.format(default))
     default = 51189318
-    parse.add_argument('--target_size', type=int, default=default,
+    group2.add_argument('--target_size', type=int, default=default,
                        help='the size of target regions for simulating short reads [{}]'.format(default))
     default = 0.5
-    parse.add_argument('--capture_efficiency', type=float, default=default,
+    group2.add_argument('--capture_efficiency', type=float, default=default,
                        help='the capture efficiency of the capture kit [{}]'.format(default))
     default = 1e6
-    parse.add_argument('--max_readnum', type=int, default=default,
+    group2.add_argument('--max_readnum', type=int, default=default,
                        help='the number of maximum short reads [{}] for a single run of simulation'.format(default))
     default = 150
-    parse.add_argument('--read_length', type=int, default=default,
+    group2.add_argument('--read_length', type=int, default=default,
                        help='Illumina: read length [{}]'.format(default))
     default = 0.5
-    parse.add_argument('-p', '--purity', type=float, default=default,
+    group2.add_argument('-p', '--purity', type=float, default=default,
                        help='the proportion of tumor cells in simulated sample [{}]'.format(default))
     default = None
-    parse.add_argument('-s', '--random_seed', type=check_seed,
+    group2.add_argument('-s', '--random_seed', type=check_seed,
                        help='the seed for random number generator [{}]'.format(default))
-    default = 'fa2wes.log'
-    parse.add_argument('-g', '--log', type=str, default=default,
-                       help='the log file to save the settings of each command [{}]'.format(default))
+    default = 'wessim'
+    group2.add_argument('--wes', type=str, default=default,
+                       help='the whole-exome sequencing simulator (available choices: wessim, capsim, capgem) used for simulating short reads [{}]'.format(default))
     default = 'snakemake --rerun-incomplete -k --latency-wait 120 --config fmedian=500'
-    parse.add_argument('--capsim', type=str, default=default,
-                       help='the parameters for calling CapSim program [{}]'.format(default))
-    args = parse.parse_args()
+    group2.add_argument('--snakemake', type=str, default=default,
+                       help='the parameters for calling a whole-exome sequencing simulator [{}]'.format(default))
+    default = False
+    group2.add_argument('--use_cluster', action='store_true', default=default,
+                   help='Run simulation on a cluster [{}]'.format(default))
+
+    group3 = parser.add_argument_group('Output options')
+    default = 'wes_reads'
+    group3.add_argument('-o', '--output', type=str, default=default,
+                       help='output directory [{}]'.format(default))
+    default = 'fa2wes.log'
+    group3.add_argument('-g', '--log', type=str, default=default,
+                       help='the log file to save the settings of each command [{}]'.format(default))
+
+
+    args = parser.parse_args()
 
     # logging and random seed setting
     logging.basicConfig(filename=args.log,
                         filemode='w', format='[%(asctime)s] %(levelname)s: %(message)s',
                         datefmt='%m-%d %H:%M:%S', level='INFO')
     logging.info(' Command: %s', ' '.join(sys.argv))
+
     if args.random_seed == None:
         seed = random_int()
     else:
@@ -323,10 +374,15 @@ def main(progname=None):
 
     check_input(args)
 
-    snake_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/Snakefile')
+    if args.wes == "capsim":
+        snake_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/Snakefile_capsim')
+    elif args.wes == "wessim":
+        snake_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/Snakefile_wessim')
+    else:
+        snake_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/Snakefile')
+
     assert os.path.isfile(snake_file), 'Cannot find Snakefile under the program directory'
-    cluster_file = os.path.join(os.path.dirname(sys.argv[0]), 'config/cluster.yaml')
-    assert os.path.isfile(cluster_file), 'Cannot find cluster.yaml under the program directory'
+
 
     normal_gsize = compute_normal_gsize(args.normal)
 
@@ -355,7 +411,7 @@ def main(progname=None):
 
         # jobs = 4 * (len(tip_node_leaves) * 2 + 2)
         jobs = total_num_splits
-        run_snakemake(outdir, args, jobs, sample_file, snake_file, cluster_file)
+        run_snakemake(outdir, args, jobs, sample_file, snake_file)
 
     if args.normal_depth > 0:
         outdir = os.path.join(args.output, 'normal')
@@ -370,4 +426,4 @@ def main(progname=None):
 
         jobs = total_num_splits
 
-        run_snakemake(outdir, args, jobs, sample_file, snake_file, cluster_file)
+        run_snakemake(outdir, args, jobs, sample_file, snake_file)
